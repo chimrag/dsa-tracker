18a4dc561dfc2e1848ba1195e194d3f5424d5ca4
chirag <107404264+chimrag@users.noreply.github.com>
2026-03-22 10:48:10 +0530

    Fix variable name from TOPICS to topics

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
 // Each section has: title, questions[]
 // Each question has: name, tag ("easy"|"medium"|"hard"), concept, leetcode (url)
 
-const TOPICS = [
+const topics = [
   {
     id: "arrays",
     name: "Arrays & Strings",
